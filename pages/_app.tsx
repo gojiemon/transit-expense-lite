e7772f36c5a92e09f2c885affa462daa468f942c
TransitExpenseLite <you@example.com>
2025-11-01 03:58:57 +0900

    chore: use single PWA icon app-icon-180.png for all references

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="manifest" href="/manifest.webmanifest" />
-        <link rel="icon" href="/icons/app-icon-192.png" type="image/png" />
+        <link rel="icon" href="/icons/app-icon-180.png" sizes="180x180" type="image/png" />
         <link rel="apple-touch-icon" href="/icons/app-icon-180.png" />
-        {/* 既存のSVGもフォールバックとして残す */}
-        <link rel="icon" href="/icons/te-icon.svg" type="image/svg+xml" />
         <meta name="theme-color" content="#4f46e5" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
